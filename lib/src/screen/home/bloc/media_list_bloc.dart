6eae30764d4c8d621db8a71eb8a1a0037b37bc99
Nguyen Van Tuyen <vuongkhaituyen@Nguyens-MBP.vmodev.com>
2020-09-22 11:18:41 +0700

    format code

--- lib/src/screen/home/bloc/media_list_bloc.dart
+++ lib/src/screen/home/bloc/media_list_bloc.dart
@@ -13,88 +13,76 @@ class MediaListBloc extends Bloc<MediaListEvent, MediaListState> {
   MediaRepository mediaRepository = MediaRepository();
 
   int mediaType = photoCode;
+  String keyWord = '';
   List<Photo> photos = [];
   List<Video> videos = [];
   int imagePage = 0;
   int videoPage = 0;
-  String keyWord = '';
 
   MediaListBloc() : super(InitialListState());
 
   @override
   Stream<MediaListState> mapEventToState(MediaListEvent event) async* {
     if (event is FetchDataEvent) {
-      yield FetchingState();
-      if (mediaType == photoCode) {
-        photos += await mediaRepository.fetchData(
-            mediaType: photoCode, page: imagePage + 1, keyWord: keyWord);
-        imagePage += 1;
-      } else {
-        videos += await mediaRepository.fetchData(
-            mediaType: videoCode, page: videoPage + 1, keyWord: keyWord);
-        videoPage += 1;
-      }
-
-      yield ShowListState(photos: photos, videos: videos, mediaType: mediaType);
+      yield* _fetchingData(mediaType);
     }
     if (event is MediaTypeChangedEvent) {
       mediaType = event.mediaType;
-      print(mediaType);
-      if (mediaType == photoCode) {
-        if (photos.isEmpty) {
-          yield FetchingState();
-          photos += await mediaRepository.fetchData(
-              mediaType: photoCode, page: imagePage + 1, keyWord: keyWord);
-          imagePage += 1;
-          print('photos.length = ${photos.length}');
-        }
-      } else {
-        if (videos.isEmpty) {
-          yield FetchingState();
-          videos += await mediaRepository.fetchData(
-              mediaType: videoCode, page: videoPage + 1, keyWord: keyWord);
-          videoPage += 1;
-        }
-      }
-      yield ShowListState(photos: photos, videos: videos, mediaType: mediaType);
+
+      yield* _fetchingInitialData(mediaType);
     }
     if (event is SearchSubmitEvent) {
       keyWord = event.keyWord;
-      photos = [];
-      videos = [];
-      imagePage = 0;
-      videoPage = 0;
-      yield FetchingState();
-      if (mediaType == photoCode) {
-        photos += await mediaRepository.fetchData(
-            mediaType: photoCode, page: imagePage + 1, keyWord: keyWord);
-        imagePage += 1;
-      } else {
-        videos += await mediaRepository.fetchData(
-            mediaType: videoCode, page: videoPage + 1, keyWord: keyWord);
-        videoPage += 1;
-      }
-      yield ShowListState(photos: photos, videos: videos, mediaType: mediaType);
+      _resetData();
+
+      yield* _fetchingData(mediaType);
     }
 
     if (event is BackToShowListEvent) {
-      photos = [];
-      videos = [];
-      imagePage = 0;
-      videoPage = 0;
       keyWord = '';
-      yield FetchingState();
-      if (mediaType == photoCode) {
+      _resetData();
+
+      yield* _fetchingData(mediaType);
+    }
+  }
+
+  void _resetData() {
+    photos = [];
+    videos = [];
+    imagePage = 0;
+    videoPage = 0;
+  }
+
+  Stream<MediaListState> _fetchingData(int mediaType) async* {
+    yield FetchingState();
+    if (mediaType == photoCode) {
+      photos += await mediaRepository.fetchData(
+          mediaType: photoCode, page: imagePage + 1, keyWord: keyWord);
+      imagePage += 1;
+    } else {
+      videos += await mediaRepository.fetchData(
+          mediaType: videoCode, page: videoPage + 1, keyWord: keyWord);
+      videoPage += 1;
+    }
+    yield ShowListState(photos: photos, videos: videos, mediaType: mediaType);
+  }
+
+  Stream<MediaListState> _fetchingInitialData(int mediaType) async* {
+    if (mediaType == photoCode) {
+      if (photos.isEmpty) {
+        yield FetchingState();
         photos += await mediaRepository.fetchData(
             mediaType: photoCode, page: imagePage + 1, keyWord: keyWord);
         imagePage += 1;
-      } else {
+      }
+    } else {
+      if (videos.isEmpty) {
+        yield FetchingState();
         videos += await mediaRepository.fetchData(
             mediaType: videoCode, page: videoPage + 1, keyWord: keyWord);
         videoPage += 1;
       }
-
-      yield ShowListState(photos: photos, videos: videos, mediaType: mediaType);
     }
+    yield ShowListState(photos: photos, videos: videos, mediaType: mediaType);
   }
 }
